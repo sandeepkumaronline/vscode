80a789a55a0897fb1527dd565e2736b0fad4fd34
Sandeep Kumar <sandeep.kumar@outlook.com>
2018-11-14 23:08:22 -0800

    Commiting

--- JavaScript/lab.js
+++ JavaScript/lab.js
@@ -1,24 +1,25 @@
+// Update greeting with user's name
 var userName = window.prompt("Please enter your name: ");
 document.getElementById("greeting").innerHTML += ", " + userName;
 
+// Get numbers from user
 var num1 = parseInt(window.prompt("Enter a number: "));
 document.getElementById("operand1").innerHTML += num1;
 
 var num2 = parseInt(window.prompt("Enter another number: "));
 document.getElementById("operand2").innerHTML += num2;
 
+// Operations
 var sum = num1 + num2;
-document.getElementById("addition").innerHTML = "The sum of " + num1 + " and " + num2 + " is: " + sum;
-
 var difference = num1 - num2;
-document.getElementById("subtraction").innerHTML = "The difference of " + num1 + " and " + num2 + " is: " + difference;
-
 var product = num1 * num2;
-document.getElementById("multiplication").innerHTML = "The product of " + num1 + " and " + num2 + " is: " + product;
-
 var quotient = num1 / num2;
-document.getElementById("division").innerHTML = "The quotient of " + num1 + " and " + num2 + " is: " + quotient;
-
 var modResult = num1 % num2;
-document.getElementById("modulus").innerHTML = "The modResult of " + num1 + " and " + num2 + " is: " + modResult;
+var numbersString = num1 + " and " + num2 + " is: ";
 
+// Display results
+document.getElementById("addition").innerHTML = "The sum of " + numbersString + sum;
+document.getElementById("subtraction").innerHTML = "The difference between " + numbersString + difference;
+document.getElementById("multiplication").innerHTML = "The product of " + numbersString + product;
+document.getElementById("division").innerHTML = "The quotient of " + numbersString + quotient;
+document.getElementById("modulus").innerHTML = "The result of the mod operation on " + numbersString + modResult;
