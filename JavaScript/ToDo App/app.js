dae96e03a8382bed536b45638fdbfda54a499307
Sandeep Kumar <sandeep.kumar@outlook.com>
2018-11-18 23:20:23 -0800

    Commiting

--- JavaScript/ToDo App/app.js
+++ JavaScript/ToDo App/app.js
@@ -2,6 +2,7 @@
 var addButton = document.getElementById("add") ;
 var taskInput = document.getElementById("task");
 var taskList = document.getElementById("taskList");
+var clearButton = document.getElementById("clear");
 
 // add new item to task list
 addButton.addEventListener("click", function(){
@@ -17,9 +18,29 @@ addButton.addEventListener("click", function(){
 
         // clear text input box
         taskInput.value = "";
+
+        // add remove option for new item
+        var removeButton = document.createElement("BUTTON");
+        removeButton.innerHTML = "Done";
+        removeButton.className = "remove";
+        removeButton.addEventListener("click", removeTask);
+        
+        newItem.appendChild(removeButton);
         taskList.appendChild(newItem);
     }
     else{
         alert("Task cannot be empty");
     }
 });
+
+// clear entire list
+clearButton.addEventListener("click", function(){
+    taskList.innerHTML = "";
+});
+
+// remove a task from the list
+function removeTask(e){
+    // get the parent list item to remove
+    var taskItem = e.target.parentElement;
+    taskList.removeChild(taskItem);
+}
